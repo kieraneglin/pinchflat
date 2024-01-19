ad6ad329c9d66ba932da7bb64a12266fd1642351
Kieran Eglin <kieran.eglin@gmail.com>
2024-01-19 12:10:43 -0800

    Updated formatting rules

--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pinchflat, Pinchflat.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: System.get_env("POSTGRES_HOST"),
   database: "pinchflat_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
