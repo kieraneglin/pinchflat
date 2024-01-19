ed317b1c2f0a5517e315be50a6bfcc6b892b903a
Kieran Eglin <kieran.eglin@gmail.com>
2024-01-18 20:46:27 -0800

    Added JS formatting rules

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  endOfLine: 'lf'
+}
