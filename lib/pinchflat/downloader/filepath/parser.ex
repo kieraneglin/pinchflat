05371cdac63600aa1101d0743257e9450f943f0c
Kieran Eglin <kieran.eglin@gmail.com>
2024-01-20 15:39:43 -0800

    More restructuring

--- lib/pinchflat/downloader/filepath/parser.ex
+++
@@ -1,7 +0,0 @@
-defmodule Pinchflat.Downloader.Filepath.Parser do
-  @moduledoc """
-  A behaviour for running CLI commands against a downloader backend
-  """
-
-  @callback run(binary(), keyword()) :: {:ok, binary()} | {:error, binary(), integer()}
-end
---
+++ lib/pinchflat/rendered_string/parser.ex
@@ -0,0 +1,5 @@
+defmodule Pinchflat.Downloader.RenderedString.Parser do
+  @moduledoc """
+  Parses liquid-ish-style strings into a rendered string
+  """
+end
