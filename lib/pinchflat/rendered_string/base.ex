7dcc893297f45923cfa9ee2bb3341971278cde45
Kieran Eglin <kieran.eglin@gmail.com>
2024-01-20 18:54:17 -0800

    Added basic parser for hydrating template strings

---
+++ lib/pinchflat/rendered_string/base.ex
@@ -0,0 +1,52 @@
+defmodule Pinchflat.RenderedString.Base do
+  @moduledoc """
+  A base module for parsing rendered strings, designed as a macro to be used
+  in other modules.
+
+  NOTE: if the needs here get any more complicated, look into using a Liquid
+  template parser. No need to reinvent the wheel any more than I already have.
+
+  NOTE: this is effectively tested by the `Pinchflat.RenderedString.Parser`'s tests
+  """
+
+  defmacro __using__(_opts) do
+    quote location: :keep do
+      import NimbleParsec
+
+      opening_tag = string("{{")
+      closing_tag = string("}}")
+      optional_whitespaces = ascii_string(~c[ \t\n\r], min: 0)
+
+      # Capture everything up to the opening object
+      text =
+        lookahead_not(opening_tag)
+        # ... as long as it's a character
+        |> utf8_char([])
+        # ... and there's at least one character
+        |> times(min: 1)
+        # ... and then convert it to a string
+        |> reduce({List, :to_string, []})
+        # ... finally bag it and tag it
+        |> unwrap_and_tag(:text)
+
+      identifier =
+        utf8_string([?a..?z, ?A..?Z, ?_, ?0..?9], min: 1)
+        |> reduce({Enum, :join, []})
+        |> unwrap_and_tag(:identifier)
+
+      defparsecp(:expression, identifier)
+
+      # when spotting interpolation, ignore the opening tag and any whitespace
+      interpolation =
+        ignore(concat(opening_tag, optional_whitespaces))
+        # ... then parse the expression (identifier)
+        |> parsec(:expression)
+        # ... then ignore any whitespace and the closing tags after the expression
+        |> ignore(concat(optional_whitespaces, closing_tag))
+        # ... once again we bag it and tag it
+        |> unwrap_and_tag(:interpolation)
+
+      defparsec(:do_parse, choice([interpolation, text]) |> repeat() |> eos())
+    end
+  end
+end
--- lib/pinchflat/rendered_string/parser.ex
+++ lib/pinchflat/rendered_string/parser.ex
@@ -1,5 +1,34 @@
-defmodule Pinchflat.Downloader.RenderedString.Parser do
+defmodule Pinchflat.RenderedString.Parser do
   @moduledoc """
   Parses liquid-ish-style strings into a rendered string
+
+  Used for turning filepath templates into real filepaths
   """
+
+  use Pinchflat.RenderedString.Base
+
+  @doc """
+  Parses a string into a rendered string, using the provided variables.
+
+  The variable keys MUST be strings.
+  """
+  def parse(string, variables) do
+    # `do_parse` comes from `RenderedString.Base`
+    case do_parse(string) do
+      {:ok, parsed, _, _, _, _} ->
+        {:ok, build_string(parsed, variables)}
+
+      {:error, message, _, _, _, _} ->
+        {:error, message}
+    end
+  end
+
+  defp build_string(parsed, variables) do
+    Enum.reduce(parsed, "", fn element, acc ->
+      case element do
+        {:text, text} -> acc <> text
+        {:interpolation, {:identifier, identifier}} -> acc <> to_string(variables[identifier])
+      end
+    end)
+  end
 end
