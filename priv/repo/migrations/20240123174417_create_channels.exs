e9e283035b2b2fb3e48f895eecab9628e634ff52
Kieran Eglin <kieran.eglin@gmail.com>
2024-02-16 09:28:42 -0800

    Edited migrations directly because I fear no man

--- priv/repo/migrations/20240123174417_create_channels.exs
+++ priv/repo/migrations/20240123174417_create_channels.exs
@@ -1,17 +1,18 @@
-defmodule Pinchflat.Repo.Migrations.CreateChannels do
+defmodule Pinchflat.Repo.Migrations.CreateSources do
   use Ecto.Migration
 
   def change do
-    create table(:channels) do
+    create table(:sources) do
       add :name, :string, null: false
-      add :channel_id, :string, null: false
+      add :collection_id, :string, null: false
+      add :collection_type, :string, null: false
       add :original_url, :string, null: false
       add :media_profile_id, references(:media_profiles, on_delete: :restrict), null: false
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:channels, [:media_profile_id])
-    create unique_index(:channels, [:channel_id, :media_profile_id])
+    create index(:sources, [:media_profile_id])
+    create unique_index(:sources, [:collection_id, :media_profile_id])
   end
 end
--- priv/repo/migrations/20240129015810_create_media_metadata.exs
+++ priv/repo/migrations/20240129015810_create_media_metadata.exs
@@ -3,13 +3,12 @@ defmodule Pinchflat.Repo.Migrations.CreateMediaMetadata do
 
   def change do
     create table(:media_metadata) do
-      add :client_response, :jsonb, null: false
+      add :client_response, :json, null: false
       add :media_item_id, references(:media_items, on_delete: :delete_all), null: false
 
       timestamps(type: :utc_datetime)
     end
 
     create unique_index(:media_metadata, [:media_item_id])
-    create index(:media_metadata, [:client_response], using: :gin)
   end
 end
