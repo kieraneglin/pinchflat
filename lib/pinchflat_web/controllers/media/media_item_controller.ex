c5fcb8fe129b05b3593fe11cd6cc254a2c841618
Kieran <kieran.eglin@gmail.com>
2024-02-15 17:51:19 -0800

    Delete media items (#20)
    * Added method for deleting media files and their content
    
    * Adds controllers and methods for deleting media and files
    
    * Improved tmpfile setup and teardown for tests
    
    * Actually got tmpfile cleanup running once per suite run
    
    * Finally fixed flash messages

--- lib/pinchflat_web/controllers/media/media_item_controller.ex
+++ lib/pinchflat_web/controllers/media/media_item_controller.ex
@@ -8,4 +8,23 @@ defmodule PinchflatWeb.Media.MediaItemController do
 
     render(conn, :show, media_item: media_item)
   end
+
+  def delete(conn, %{"id" => id} = params) do
+    delete_files = Map.get(params, "delete_files", false)
+    media_item = Media.get_media_item!(id)
+
+    if delete_files do
+      {:ok, _} = Media.delete_media_item_and_attachments(media_item)
+
+      conn
+      |> put_flash(:info, "Record and files deleted successfully.")
+      |> redirect(to: ~p"/sources/#{media_item.source_id}")
+    else
+      {:ok, _} = Media.delete_media_item(media_item)
+
+      conn
+      |> put_flash(:info, "Record deleted successfully. Files were not deleted.")
+      |> redirect(to: ~p"/sources/#{media_item.source_id}")
+    end
+  end
 end
