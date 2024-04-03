3b1c1692fbb5c64d5dd4bb79dce83832df3f71e3
Kieran <kieran.eglin@gmail.com>
2024-04-03 11:05:49 -0700

    Retained tab state using location hash (#161)

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,7 @@ import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import topbar from '../vendor/topbar'
 import Alpine from 'alpinejs'
+import './tabs'
 
 window.Alpine = Alpine
 Alpine.start()
