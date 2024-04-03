4b12764f453897fb9adb6a4bd34a7afc08b49d1d
Kieran <kieran.eglin@gmail.com>
2024-04-03 14:57:45 -0700

    [Housekeeping] Upgraded Tailwind to 3.4.3 (#163)
    * Upgraded Tailwind
    
    * Enabled asset compression in prod

--- lib/pinchflat_web/endpoint.ex
+++ lib/pinchflat_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule PinchflatWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :pinchflat,
-    gzip: false,
+    gzip: Mix.env() == :prod,
     only: PinchflatWeb.static_paths()
 
   # Code reloading can be explicitly enabled under the
