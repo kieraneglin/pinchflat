2dfdb31aa43043ac562e3ccc4d762a3faa2a8af0
Kieran Eglin <kieran.eglin@gmail.com>
2024-05-14 11:03:36 -0700

    Fixed release tag hopefully

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pinchflat.MixProject do
   def project do
     [
       app: :pinchflat,
-      version: "2024.05.14",
+      version: "2024.5.14",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
