d10f189f133e3b768dd18bc47d5c3d01b444dc15
Kieran Eglin <kieran.eglin@gmail.com>
2024-05-15 12:13:27 -0700

    Added new view for getting diagnostic data

--- test/pinchflat_web/controllers/setting_controller_test.exs
+++ test/pinchflat_web/controllers/setting_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule PinchflatWeb.SettingControllerTest do
   use PinchflatWeb.ConnCase
 
+  alias Pinchflat.Utils.FilesystemUtils
+
   describe "show settings" do
     test "renders the page", %{conn: conn} do
       conn = get(conn, ~p"/settings")
@@ -18,6 +20,35 @@ defmodule PinchflatWeb.SettingControllerTest do
 
       conn = get(conn, ~p"/settings")
       assert html_response(conn, 200) =~ update_attrs[:apprise_server]
+    end
+  end
+
+  describe "app_info" do
+    test "renders the page", %{conn: conn} do
+      conn = get(conn, ~p"/app_info")
+
+      assert html_response(conn, 200) =~ "App Info"
+    end
+  end
+
+  describe "download_logs" do
+    test "downloads logs", %{conn: conn} do
+      log_path = Path.join([System.tmp_dir!(), "pinchflat", "data", "pinchflat.log"])
+      FilesystemUtils.write_p(log_path, "test log data")
+      Application.put_env(:pinchflat, :log_path, log_path)
+
+      conn = get(conn, ~p"/download_logs")
+
+      assert response(conn, 200) =~ "test log data"
+
+      Application.put_env(:pinchflat, :log_path, nil)
+    end
+
+    test "redirects when log file is not found", %{conn: conn} do
+      conn = get(conn, ~p"/download_logs")
+
+      assert redirected_to(conn) == ~p"/app_info"
+      assert conn.assigns[:flash]["error"] == "Log file couldn't be found"
     end
   end
 end
