cb79d24fb93dbe8ab3c2c340493dae907f9f9186
Kieran Eglin <kieran.eglin@gmail.com>
2024-05-23 15:03:57 -0700

    Improved centering for media table header elements

--- lib/pinchflat_web/controllers/sources/source_html/media_item_table_live.ex
+++ lib/pinchflat_web/controllers/sources/source_html/media_item_table_live.ex
@@ -20,8 +20,8 @@ defmodule Pinchflat.Sources.MediaItemTableLive do
   def render(assigns) do
     ~H"""
     <div>
-      <header class="flex justify-between items-center">
-        <span class="mb-4 flex items-center">
+      <header class="flex justify-between items-center mb-4">
+        <span class="flex items-center">
           <.icon_button icon_name="hero-arrow-path" class="h-10 w-10" phx-click="reload_page" tooltip="Refresh" />
           <span class="ml-2">Showing <%= length(@records) %> of <%= @filtered_record_count %></span>
         </span>
