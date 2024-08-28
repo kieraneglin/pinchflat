e3f42c28d0b0834f7a48b1376ca38aafa0375eb3
Kieran Eglin <kieran.eglin@gmail.com>
2024-08-27 20:25:53 -0700

    Opps - reversed the IPv6 logic

--- config/runtime.exs
+++ config/runtime.exs
@@ -121,7 +121,7 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: if(enable_ipv6, do: {0, 0, 0, 0}, else: {0, 0, 0, 0, 0, 0, 0, 0}),
+      ip: if(enable_ipv6, do: {0, 0, 0, 0, 0, 0, 0, 0}, else: {0, 0, 0, 0}),
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     url: [path: base_route_path],
