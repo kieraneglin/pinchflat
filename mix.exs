20547e60e4adb51760364824c00091ef4a8b8224
Kieran Eglin <kieran.eglin@gmail.com>
2024-10-25 11:42:16 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pinchflat.MixProject do
   def project do
     [
       app: :pinchflat,
-      version: "2024.10.2",
+      version: "2024.10.25",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
