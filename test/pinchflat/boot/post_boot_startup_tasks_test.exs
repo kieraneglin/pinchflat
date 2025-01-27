91f9bcf2af7cb756dd24c8a83ea60b1bdd360f65
Kieran Eglin <kieran.eglin@gmail.com>
2025-01-27 11:25:33 -0800

    Removes unneeded test code

--- test/pinchflat/boot/post_boot_startup_tasks_test.exs
+++ test/pinchflat/boot/post_boot_startup_tasks_test.exs
@@ -1,18 +1,8 @@
 defmodule Pinchflat.Boot.PostBootStartupTasksTest do
   use Pinchflat.DataCase
 
-  # import Pinchflat.JobFixtures
-
-  # alias Pinchflat.Settings
   alias Pinchflat.YtDlp.UpdateWorker
   alias Pinchflat.Boot.PostBootStartupTasks
-
-  setup do
-    stub(YtDlpRunnerMock, :update, fn -> {:ok, "1"} end)
-    stub(YtDlpRunnerMock, :version, fn -> {:ok, "1"} end)
-
-    :ok
-  end
 
   describe "update_yt_dlp" do
     test "enqueues an update job" do
