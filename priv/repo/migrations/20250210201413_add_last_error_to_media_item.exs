e65d27026099a23c9afd3f44f1d143c6e8842ccf
Kieran Eglin <kieran.eglin@gmail.com>
2025-02-11 14:43:46 -0800

    Remove unneeded index

--- priv/repo/migrations/20250210201413_add_last_error_to_media_item.exs
+++ priv/repo/migrations/20250210201413_add_last_error_to_media_item.exs
@@ -5,8 +5,5 @@ defmodule Pinchflat.Repo.Migrations.AddLastErrorToMediaItem do
     alter table(:media_items) do
       add :last_error, :string
     end
-
-    execute "CREATE INDEX media_items_last_error_index ON media_items (last_error);",
-            "DROP INDEX media_items_last_error_index;"
   end
 end
